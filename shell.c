d4adea894daecaf7e6b512b51733cd0cb1efb442
NChen2018 <46333358+NChen2018@users.noreply.github.com>
2020-01-08 07:19:07 -0800

    Add files via upload

---
+++ shell.c
@@ -0,0 +1,23 @@
+#define LSH_RL_BUFSIZE 1024
+int main(int argc, char** argc) {
+	lsh_loop();
+	return EXIT_SUCCESS;
+}
+
+void lsh_loop() {
+	char *line;
+	char **args;
+	int status;
+
+	do{
+		printf("> ");
+		line = lsh_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+	}while(status);
+}
+
+
